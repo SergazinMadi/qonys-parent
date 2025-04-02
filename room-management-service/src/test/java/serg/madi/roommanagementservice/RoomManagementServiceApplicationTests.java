b7a70b68950b5c7cd7b2101dd3688ec8d13b1af6
Sausaq <nadikz4444@gmail.com>
2025-04-02 22:27:05 +0500

    Initial commit

---
+++ room-management-service/src/test/java/serg/madi/roommanagementservice/RoomManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package serg.madi.roommanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomManagementServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
